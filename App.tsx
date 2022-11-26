60e733dcb9825cff48417d2d0735fc6d839f68ac
Murat Çelik <cashkl@yandex.com>
2022-11-26 20:25:02 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/screens/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Home;
